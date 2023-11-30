a0bb416161ecf76d3f5474782dfc6f8876e65a1e
Alex Gulliver <randdalf@gmail.com>
2023-11-30 22:25:21 +0000

    Implement full wrapper executable for running solvers

--- aoc.c
+++ aoc.c
@@ -1,13 +1,46 @@
 #include "aoc.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 extern const char* DefaultInputPath;
 extern AOC_SOLVER(Part1);
 extern AOC_SOLVER(Part2);
 
-static const char* ReadInputFile(const char* Path)
+static char* ReadStandardInput(void)
+{
+    size_t Length = 0;
+    size_t Capacity = 128;
+    char* Chars = (char*)malloc(Capacity);
+    Chars[0] = '\0';
+    char Buffer[128];
+    while(fgets(Buffer, sizeof(Buffer), stdin))
+    {
+        size_t BufferLength = strlen(Buffer) + 1;
+        size_t OldCapacity = Capacity;
+        while(Length + BufferLength > Capacity)
+        {
+            Capacity *= 2;
+        }
+        if(Capacity > OldCapacity)
+        {
+            Chars = (char*)realloc(Chars, Capacity);
+        }
+        memcpy(Chars + Length, Buffer, BufferLength);
+        Length += BufferLength - 1;
+    }
+    if(!feof(stdin))
+    {
+        free(Chars);
+        return NULL;
+    }
+    return Chars;
+}
+
+static char* ReadInputFile(const char* Path)
 {
     FILE* File = fopen(Path, "rb");
     if(File == NULL) return NULL;
@@ -26,10 +59,102 @@ static const char* ReadInputFile(const char* Path)
     return Buffer;
 }
 
-int main(int ArgCount, char** Args)
+static void PrintUsage(const char* Prog)
 {
-    const char* Input = ReadInputFile(DefaultInputPath);
-    if(!Input) return EXIT_FAILURE;
-    printf("%s\n", Input);
+    printf("usage: %s [<options>]\n\n", Prog);
+    printf("    -1  run part 1 only\n");
+    printf("    -2  run part 2 only\n");
+    printf("    -h  print this help\n");
+    printf("    -i  read input from stdin\n");
+    printf("    -q  quiet mode\n");
+    printf("    -e  echo puzzle input to stdout, then exit\n");
+}
+
+static void RunSolver(aoc_solver* Solver, const char* Input, bool Quiet)
+{
+    clock_t Start = clock();
+    int64_t Result = Solver(Input);
+    clock_t End = clock();
+    if(Result == -1) return;
+    printf("%-30lld", Result);
+    if(!Quiet)
+    {
+        printf(" %.4fs", (double)(End - Start) / CLOCKS_PER_SEC);
+    }
+    printf("\n");
+}
+
+int main(int ArgCount, const char** Args)
+{
+    // Parse command line arguments.
+    int ExclusiveSolver = -1;
+    bool EchoInput = false;
+    bool UseStandardInput = false;
+    bool Quiet = false;
+    for(int ArgIndex = 1; ArgIndex < ArgCount; ArgIndex++)
+    {
+        const char* Arg = Args[ArgIndex];
+        if(Arg[0] != '-')
+        {
+            PrintUsage(Args[0]);
+            return EXIT_FAILURE;
+        }
+        int Index = 1;
+        for(int CharIndex = 1; Arg[CharIndex] != '\0'; CharIndex++)
+        {
+            switch(Arg[CharIndex])
+            {
+            case '1': ExclusiveSolver = 1; break;
+            case '2': ExclusiveSolver = 2; break;
+            case 'e': EchoInput = true; break;
+            case 'i': UseStandardInput = true; break;
+            case 'h':
+                PrintUsage(Args[0]);
+                return EXIT_SUCCESS;
+            case 'q': Quiet = true; break;
+            default:
+                PrintUsage(Args[0]);
+                return EXIT_FAILURE;
+            }
+        }
+    }
+
+    // Read puzzle input.
+    char* Input;
+    if(UseStandardInput)
+    {
+        Input = ReadStandardInput();
+    }
+    else
+    {
+        Input = ReadInputFile(DefaultInputPath);
+    }
+    if(!Input)
+    {
+        fprintf(stderr, "Failed to read input.");
+        return EXIT_FAILURE;
+    }
+
+    // Echo input and exit, if instructed.
+    if(EchoInput)
+    {
+        puts(Input);
+        return EXIT_SUCCESS;
+    }
+
+    // Run part 1.
+    if(ExclusiveSolver < 0 || ExclusiveSolver == 1)
+    {
+        RunSolver(Part1, Input, Quiet);
+    }
+
+    // Run part 2.
+    if(ExclusiveSolver < 0 || ExclusiveSolver == 2)
+    {
+        RunSolver(Part2, Input, Quiet);
+    }
+
+    // Tidy up and return.
+    free(Input);
     return EXIT_SUCCESS;
 }
--- aoc.h
+++ aoc.h
@@ -3,3 +3,4 @@
 #include <stdint.h>
 
 #define AOC_SOLVER(Name) int64_t Name(const char* Input)
+typedef AOC_SOLVER(aoc_solver);
