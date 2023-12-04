4a3111d45057407ac3a3fd3ac1588fdc8c92295a
Alex Gulliver <randdalf@gmail.com>
2023-12-04 21:04:56 +0000

    Rewrite Day 4 to share parsing logic between solutions

--- d04.c
+++ d04.c
@@ -11,21 +11,18 @@ static bool IsDigit(char C)
     return C >= '0' && C <= '9';
 }
 
-AOC_SOLVER(Part1)
+static const char* Next(const char* Input, int* OutMatches)
 {
-    int64_t Sum = 0;
-    uint64_t Lo, Hi;
-    int Number, Matches;
-    bool Winning;
+    uint64_t Lo = 0, Hi = 0;
+    int Number, Matches = 0;
+    bool Winning = true;
     char C;
-    do
+    for(;;)
     {
         C = *Input++;
         switch(C)
         {
         case 'C':
-            Lo = Hi = Matches = 0;
-            Winning = true;
             Input += 4;
             while(*Input == ' ') Input++;
             while(IsDigit(*Input)) Input++;
@@ -66,95 +63,55 @@ AOC_SOLVER(Part1)
             Input++;
             break;
         case '\n':
+            *OutMatches = Matches;
+            return Input;
         case '\0':
-            if(Matches) Sum += 1 << (Matches - 1);
-            break;
+            *OutMatches = Matches;
+            return NULL;
         }
-    } while(C != '\0');
+    }
+}
+
+AOC_SOLVER(Part1)
+{
+    int64_t Sum = 0;
+    int Matches;
+    do
+    {
+        Input = Next(Input, &Matches);
+        if(Matches) Sum += 1 << (Matches - 1);
+    } while(Input);
     return Sum;
 }
 
 AOC_SOLVER(Part2)
 {
     int64_t Sum = 0;
-    uint64_t Lo, Hi;
-    int Number, Matches;
-    int CardIndex = 0;
-    int Capacity = 256;
+    int Matches;
+    int CardIndex = 0, Capacity = 256;
     int* Copies = (int*)calloc(Capacity, sizeof(int));
-    bool Winning;
-    char C;
     do
     {
-        C = *Input++;
-        switch(C)
+        Input = Next(Input, &Matches);
+        int NumCards = 1;
+        if(CardIndex < Capacity)
         {
-        case 'C':
-            Lo = Hi = Matches = 0;
-            Winning = true;
-            Input += 4;
-            while(*Input == ' ') Input++;
-            while(IsDigit(*Input)) Input++;
-            Input += 2;
-            break;
-        case '0': case '1': case '2': case '3': case '4':
-        case '5': case '6': case '7': case '8': case '9':
-            Number = atoi(Input - 1);
-            while(IsDigit(*Input)) Input++;
-            if(Winning)
-            {
-                if(Number >= 64)
-                {
-                    Hi |= 1llu << (Number - 64);
-                }
-                else
-                {
-                    Lo |= 1llu << Number;
-                }
-            }
-            else
-            {
-                uint64_t Set;
-                if(Number >= 64)
-                {
-                    Set = Hi;
-                    Number -= 64;
-                }
-                else
-                {
-                    Set = Lo;
-                }
-                if(Set & (1llu << Number)) Matches++;
-            }
-            break;
-        case '|':
-            Winning = false;
-            Input++;
-            break;
-        case '\n':
-        case '\0':
-            int NumCards = 1;
-            if(CardIndex < Capacity)
-            {
-                NumCards += Copies[CardIndex];
-            }
-            Sum += NumCards;
-            for(int Match = 1; Match <= Matches; Match++)
-            {
-                int CopyIndex = CardIndex + Match;
-                while(CopyIndex >= Capacity)
-                {
-                    size_t HalfSize = sizeof(int) * Capacity;
-                    Capacity *= 2;
-                    Copies = (int*)realloc(Copies, sizeof(int) * Capacity);
-                    memset(Copies + CopyIndex, 0, HalfSize);
-                }
-                Copies[CopyIndex] += NumCards;
-            }
-            CardIndex++;
-            break;
+            NumCards += Copies[CardIndex];
         }
-    } while(C != '\0');
-    free(Copies);
+        Sum += NumCards;
+        for(int Match = 1; Match <= Matches; Match++)
+        {
+            int CopyIndex = CardIndex + Match;
+            while(CopyIndex >= Capacity)
+            {
+                size_t HalfSize = sizeof(int) * Capacity;
+                Capacity *= 2;
+                Copies = (int*)realloc(Copies, sizeof(int) * Capacity);
+                memset(Copies + CopyIndex, 0, HalfSize);
+            }
+            Copies[CopyIndex] += NumCards;
+        }
+        CardIndex++;
+    } while(Input);
     return Sum;
 }
