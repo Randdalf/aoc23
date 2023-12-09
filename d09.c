59d03fe1330f541253f1c09c86eeaccc7468c340
Alex Gulliver <randdalf@gmail.com>
2023-12-09 10:09:34 +0000

    Day 9, Puzzle 1

---
+++ d09.c
@@ -0,0 +1,102 @@
+#include "aoc.h"
+#include "parse.h"
+
+const char* DefaultInputPath = "d09.txt";
+
+typedef struct
+{
+    int64_t* Elements;
+    size_t Count;
+    size_t Capacity;
+} sequence;
+
+static void InitSequence(sequence* Sequence)
+{
+    Sequence->Elements = NULL;
+    Sequence->Count = 0;
+    Sequence->Capacity = 0;
+}
+
+static void FreeSequence(sequence* Sequence)
+{
+    free(Sequence->Elements);
+    InitSequence(Sequence);
+}
+
+static void SequenceAdd(sequence* Sequence, int64_t Element)
+{
+    size_t Capacity = Sequence->Capacity;
+    if(Sequence->Count == Capacity)
+    {
+        Capacity = Capacity ? 2 * Capacity : 8;
+        Sequence->Elements = (int64_t*)realloc(Sequence->Elements, sizeof(int64_t) * Capacity);
+        Sequence->Capacity = Capacity;
+    }
+    Sequence->Elements[Sequence->Count++] = Element;
+}
+
+static void SequenceReset(sequence* Sequence)
+{
+    Sequence->Count = 0;
+}
+
+static bool SequenceIsZero(sequence* Sequence)
+{
+    for(int Index = 0; Index < Sequence->Count; Index++)
+    {
+        if(Sequence->Elements[Index])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool IsNumeric(char C)
+{
+    return IsDigit(C) || C == '-';
+}
+
+static inline const char* SkipPastNumeric(const char* Input)
+{
+    while(IsNumeric(*Input)) Input++;
+    return Input;
+}
+
+AOC_SOLVER(Part1)
+{
+    int64_t Sum = 0;
+    sequence Sequence;
+    InitSequence(&Sequence);
+    while(IsNumeric(*Input))
+    {
+        // Parse the sequence.
+        while(IsNumeric(*Input))
+        {
+            SequenceAdd(&Sequence, atoll(Input));
+            Input = SkipPastNumeric(Input);
+            Input = SkipPastWhitespace(Input);
+        }
+        Input = SkipPastNewline(Input);
+
+        // Reduce the sequence to zero, adding the end of each step to the sum.
+        // (As they will sum the next number in the sequence)
+        while(!SequenceIsZero(&Sequence))
+        {
+            Sum += Sequence.Elements[Sequence.Count - 1];
+            for(int Index = 1; Index < Sequence.Count; Index++)
+            {
+                Sequence.Elements[Index - 1] = Sequence.Elements[Index] - Sequence.Elements[Index - 1];
+            }
+            Sequence.Count--;
+        }
+
+        SequenceReset(&Sequence);
+    }
+    return Sum;
+}
+
+AOC_SOLVER(Part2)
+{
+    return -1;
+}
