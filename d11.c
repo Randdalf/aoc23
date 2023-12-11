6c95e3bb29846e8bd9e473816ca5a6d97f8e676f
Alex Gulliver <randdalf@gmail.com>
2023-12-11 19:08:59 +0000

    Day 11, Puzzle 1

---
+++ d11.c
@@ -0,0 +1,148 @@
+#include "aoc.h"
+#include "parse.h"
+
+const char* DefaultInputPath = "d11.txt";
+
+static bool IsEmpty(char C)
+{
+    return C == '.';
+}
+
+static bool IsGalaxy(char C)
+{
+    return C == '#';
+}
+
+static bool IsImage(char C)
+{
+    return IsEmpty(C) || IsGalaxy(C);
+}
+
+static const char* SkipPastEmpty(const char* Input)
+{
+    while(IsEmpty(*Input)) Input++;
+    return Input;
+}
+
+typedef struct
+{
+    uint64_t Bits[3];
+} bit_set;
+
+static void InitBitSet(bit_set* Set, uint8_t Byte)
+{
+    memset(Set, Byte, sizeof(bit_set));
+}
+
+static void BitSetClear(bit_set* Set, uint64_t Index)
+{
+    Set->Bits[Index / 64llu] &= ~(1llu << (Index % 64llu));
+}
+
+static bool BitSetContains(bit_set* Set, uint64_t Index)
+{
+    return (Set->Bits[Index / 64llu] & (1llu << (Index % 64llu))) != 0;
+}
+
+static int BitSetCount(bit_set* Set)
+{
+    return (int)(__popcnt64(Set->Bits[0]) + __popcnt64(Set->Bits[1]) + __popcnt64(Set->Bits[2]));
+}
+
+typedef struct
+{
+    int X;
+    int Y;
+} ivec2;
+
+AOC_SOLVER(Part1)
+{
+    int Expansion = 2;
+
+    // Scan the input, to determine which rows are empty and which contain
+    // galaxies.
+    bit_set EmptyRows, EmptyCols;
+    InitBitSet(&EmptyRows, 0xFF);
+    InitBitSet(&EmptyCols, 0xFF);
+    int GalaxyCount = 0;
+    const char* At = Input;
+    int Row = 0;
+    while(IsImage(*At))
+    {
+        int Col = 0;
+        while(IsImage(*At))
+        {
+            if(IsGalaxy(*At))
+            {
+                BitSetClear(&EmptyCols, Col);
+                BitSetClear(&EmptyRows, Row);
+                GalaxyCount++;
+            }
+            Col++;
+            At++;
+        }
+        At = SkipPastNewline(At);
+        Row++;
+    }
+
+    // Scan the input again, calculating the positions of the galaxies with the
+    // rows expanded.
+    ivec2* Galaxies = (ivec2*)malloc(sizeof(ivec2) * GalaxyCount);
+    GalaxyCount = 0;
+    Row = 0;
+    At = Input;
+    int ExpandedRow = 0;
+    while(IsImage(*At))
+    {
+        if(BitSetContains(&EmptyRows, Row))
+        {
+            At = SkipPastEmpty(At);
+            ExpandedRow += Expansion;
+        }
+        else
+        {
+            int Col = 0;
+            int ExpandedCol = 0;
+            while(IsImage(*At))
+            {
+                if(BitSetContains(&EmptyCols, Col))
+                {
+                    ExpandedCol += Expansion;
+                }
+                else
+                {
+                    if(IsGalaxy(*At))
+                    {
+                        Galaxies[GalaxyCount++] = (ivec2){.X = ExpandedCol, .Y = ExpandedRow};
+                    }
+                    ExpandedCol++;
+                }
+                Col++;
+                At++;
+            }
+            ExpandedRow++;
+        }
+        At = SkipPastNewline(At);
+        Row++;
+    }
+
+    // Calculate the manhattan distance between galaxies.
+    uint64_t Sum = 0;
+    for(int FromIndex = 0; FromIndex < GalaxyCount; FromIndex++)
+    {
+        ivec2 From = Galaxies[FromIndex];
+        for(int ToIndex = FromIndex + 1; ToIndex < GalaxyCount; ToIndex++)
+        {
+            ivec2 To = Galaxies[ToIndex];
+            Sum += abs(To.X - From.X) + abs(To.Y - From.Y);
+        }
+    }
+
+    free(Galaxies);
+    return Sum;
+}
+
+AOC_SOLVER(Part2)
+{
+    return -1;
+}
