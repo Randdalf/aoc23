5ae94e7014ae7ef6c4c20738db06c9a3326604d2
Alex Gulliver <randdalf@gmail.com>
2023-12-13 21:08:37 +0000

    Day 13, Puzzle 2

--- d13.c
+++ d13.c
@@ -54,10 +54,13 @@ static void SliceArrayReset(slice_array* Array)
     Array->Count = 0;
 }
 
-static int CheckReflection(slice_array* Slices)
+typedef int (*check_reflection_fn)(slice_array* Slices, int Width);
+
+static int CheckReflectionWithSkip(slice_array* Slices, int Width, int SkipSliceIndex)
 {
     for(int SliceIndex = 0; SliceIndex < Slices->Count - 1; SliceIndex++)
     {
+        if(SliceIndex == SkipSliceIndex) continue;
         int LeftIndex = SliceIndex;
         int RightIndex = SliceIndex + 1;
         for(; LeftIndex >= 0 && RightIndex < Slices->Count; RightIndex++, LeftIndex--)
@@ -71,13 +74,36 @@ static int CheckReflection(slice_array* Slices)
     return 0;
 }
 
-AOC_SOLVER(Part1)
+static int CheckReflection(slice_array* Slices, int Width)
+{
+    return CheckReflectionWithSkip(Slices, Width, -1);
+}
+
+static int CheckReflectionWithSmudge(slice_array* Slices, int Width)
+{
+    int SkipSliceIndex = CheckReflection(Slices, Width) - 1;
+    for(int SliceIndex = 0; SliceIndex < Slices->Count; SliceIndex++)
+    {
+        for(int BitIndex = 0; BitIndex < Width; BitIndex++)
+        {
+            uint32_t Flipper = 1 << BitIndex;
+            Slices->Elements[SliceIndex] ^= Flipper;
+            int Result = CheckReflectionWithSkip(Slices, Width, SkipSliceIndex);
+            Slices->Elements[SliceIndex] ^= Flipper;
+            if(Result) return Result;
+        }
+    }
+    return 0;
+}
+
+static int64_t Solve(const char* Input, check_reflection_fn CheckReflectionFn)
 {
     int64_t SumCols = 0;
     int64_t SumRows = 0;
     slice_array Rows, Cols;
     InitSliceArray(&Rows);
     InitSliceArray(&Cols);
+    int PatternNum = 0;
     while(IsPattern(*Input))
     {
         int Row = 0;
@@ -104,8 +130,8 @@ AOC_SOLVER(Part1)
             Input = SkipPastNewline(Input);
             Row++;
         }
-        SumCols += CheckReflection(&Cols);
-        SumRows += CheckReflection(&Rows);
+        SumCols += CheckReflectionFn(&Cols, Rows.Count);
+        SumRows += CheckReflectionFn(&Rows, Cols.Count);
         SliceArrayReset(&Rows);
         SliceArrayReset(&Cols);
         Input = SkipPastNewline(Input);
@@ -115,7 +141,12 @@ AOC_SOLVER(Part1)
     return SumCols + 100 * SumRows;
 }
 
+AOC_SOLVER(Part1)
+{
+    return Solve(Input, CheckReflection);
+}
+
 AOC_SOLVER(Part2)
 {
-    return -1;
+    return Solve(Input, CheckReflectionWithSmudge);
 }
