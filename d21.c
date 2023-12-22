305f4a05decdbb0ac1bbb98a341229412a10c478
Alex Gulliver <randdalf@gmail.com>
2023-12-22 23:29:51 +0000

    Rework Day 21, Puzzle 1 to be faster but more verbose

--- d21.c
+++ d21.c
@@ -64,17 +64,16 @@ static void InitGrid(grid* Grid, const char* Input)
 
 typedef struct
 {
-    int X;
-    int Y;
-    int StepsRemaining;
+    int64_t X;
+    int64_t Y;
 } key;
 
-typedef int64_t value;
+typedef uint8_t value;
 
 typedef struct
 {
-    int Count;
-    int Capacity;
+    size_t Count;
+    size_t Capacity;
     uint8_t* Slots;
     key* Keys;
     value* Values;
@@ -154,56 +153,8 @@ static void TableSet(table* Table, key Key, value Value)
     }
 }
 
-static bool TableGet(table* Table, key Key, value* Value)
+static int64_t Solve(const char* Input, int64_t NumSteps)
 {
-    if(Table->Count == 0) return false;
-    uint64_t Hash = (int)XXH3_64bits(&Key, sizeof(key));
-    int CapacityMask = Table->Capacity - 1;
-    int Index = Hash & CapacityMask;
-    for(;;)
-    {
-        if(Table->Slots[Index])
-        {
-            if(!memcmp(&Table->Keys[Index], &Key, sizeof(key)))
-            {
-                *Value = Table->Values[Index];
-                return true;
-            }
-        }
-        else
-        {
-            return false;
-        }
-        Index = (Index + 1) & CapacityMask;
-    }
-}
-
-static int64_t CountPlots(table* Cache, grid* Grid, int X, int Y, int StepsRemaining)
-{
-    key Key = (key){.X = X, .Y = Y, .StepsRemaining = StepsRemaining};
-    int64_t Value = 0;
-    if(X < 0 || X >= Grid->Width) return 0;
-    if(Y < 0 || Y >= Grid->Height) return 0;
-    int Index = Y * Grid->Width + X;
-    if(Grid->Cells[Index] == '#') return 0;
-    if(TableGet(Cache, Key, &Value)) return 0;
-    if(!StepsRemaining)
-    {
-        TableSet(Cache, Key, 1);
-        return 1;
-    }
-    StepsRemaining--;
-    Value += CountPlots(Cache, Grid, X, Y - 1, StepsRemaining);
-    Value += CountPlots(Cache, Grid, X + 1, Y, StepsRemaining);
-    Value += CountPlots(Cache, Grid, X, Y + 1, StepsRemaining);
-    Value += CountPlots(Cache, Grid, X - 1, Y, StepsRemaining);
-    TableSet(Cache, Key, 1);
-    return Value;
-}
-
-AOC_SOLVER(Part1)
-{
-    int NumSteps = 64;
     if(IsDigit(*Input))
     {
         NumSteps = 0;
@@ -212,10 +163,61 @@ AOC_SOLVER(Part1)
     }
     grid Grid;
     InitGrid(&Grid, Input);
-    table Cache;
-    InitTable(&Cache);
-    int64_t Result = CountPlots(&Cache, &Grid, Grid.Start.X, Grid.Start.Y, NumSteps);
-    return Result;
+    table Tables[2];
+    InitTable(&Tables[0]);
+    InitTable(&Tables[1]);
+    int TableIndex = 0;
+    TableSet(&Tables[TableIndex], (key){.X = Grid.Start.X, .Y = Grid.Start.Y}, 1);
+    for(int64_t Step = 0; Step < NumSteps; Step++)
+    {
+        table* From = &Tables[TableIndex];
+        table* To = &Tables[1 - TableIndex];
+        TableReset(To);
+        TableIndex = 1 - TableIndex;
+        for(int Index = 0; Index < From->Capacity; Index++)
+        {
+            if(!From->Slots[Index]) continue;
+            key Key = From->Keys[Index];
+            if(Key.Y > 0)
+            {
+                key North = (key){.X = Key.X, .Y = Key.Y - 1};
+                if(Grid.Cells[North.Y * Grid.Width + North.X] != '#')
+                {
+                    TableSet(To, North, 1);
+                }
+            }
+            if(Key.X < Grid.Width - 1)
+            {
+                key East = (key){.X = Key.X + 1, .Y = Key.Y};
+                if(Grid.Cells[East.Y * Grid.Width + East.X] != '#')
+                {
+                    TableSet(To, East, 1);
+                }
+            }
+            if(Key.Y < Grid.Height - 1)
+            {
+                key South = (key){.X = Key.X, .Y = Key.Y + 1};
+                if(Grid.Cells[South.Y * Grid.Width + South.X] != '#')
+                {
+                    TableSet(To, South, 1);
+                }
+            }
+            if(Key.X > 0)
+            {
+                key West = (key){.X = Key.X - 1, .Y = Key.Y};
+                if(Grid.Cells[West.Y * Grid.Width + West.X] != '#')
+                {
+                    TableSet(To, West, 1);
+                }
+            }
+        }
+    }
+    return Tables[TableIndex].Count;
+}
+
+AOC_SOLVER(Part1)
+{
+    return Solve(Input, 64);
 }
 
 AOC_SOLVER(Part2)
